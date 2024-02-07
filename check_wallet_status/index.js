e4b85e75bef9bf2f021574bc914b7ee5cf5aaa5b
Seman Arda Yuvarlak <ardayuvarlak@gmail.com>
2024-02-07 10:05:22 -0800

    Update index.js

--- check_wallet_status/index.js
+++ check_wallet_status/index.js
@@ -1,18 +1,1 @@
-import dotenv from "dotenv";
-import fetch from "node-fetch";
 
-dotenv.config();
-
-const url = `https://api.circle.com/v1/w3s/wallets?userId=${process.env.USER_ID_R}`;
-const options = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.API_KEY_R}`,
-  },
-};
-
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(JSON.stringify(json, null, 2)))
-  .catch((err) => console.error("error:" + err));
