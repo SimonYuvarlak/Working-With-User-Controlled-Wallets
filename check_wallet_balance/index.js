5cfacba6053ac541f2551efc513ed42761434532
Seman Arda Yuvarlak <ardayuvarlak@gmail.com>
2024-02-07 10:05:07 -0800

    Update index.js

--- check_wallet_balance/index.js
+++ check_wallet_balance/index.js
@@ -1,18 +1,1 @@
-import dotenv from "dotenv";
-import fetch from "node-fetch";
 
-dotenv.config();
-
-const url = `https://api.circle.com/v1/w3s/wallets/${process.env.WALLET_ID}/balances`;
-const options = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.API_KEY}`,
-  },
-};
-
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.dir(json, { depth: null }))
-  .catch((err) => console.error("error:" + err));
